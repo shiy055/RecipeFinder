9530cd10e985ac2ed62fd3cf3a4030f2bfac2880
Cathy <yinjies@umich.edu>
2025-04-22 17:40:48 +0000

    edit 8

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,51 +1,27 @@
 // Ingredient input field
 
 import React from 'react';
-import Autocomplete from '@mui/material/Autocomplete';
-import TextField from '@mui/material/TextField';
-import recipesData from '../data/recipesData';
 
 const SearchBar = ({ onSearch }) => {
-  // const handleSubmit = (e) => {
-  //   e.preventDefault();
-  //   const input = e.target.elements.ingredients.value;
-  //   onSearch(input);
-  // };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const input = e.target.elements.ingredients.value;
+    onSearch(input);
+  };
 
-  // return (
-  //   <form onSubmit={handleSubmit} className="mb-4">
-  //     <div className="input-group">
-  //       <input
-  //         type="text"
-  //         name="ingredients"
-  //         className="form-control"
-  //         placeholder="Enter ingredients (e.g., tomato, basil)"
-  //       />
-  //       <button type="submit" className="btn btn-primary">Find Recipes</button>
-  //     </div>
-  //   </form>
-  // );
   return (
-    <Autocomplete
-      freeSolo
-      id="recipe-search"
-      disableClearable
-      options={recipesData.map((item) => item.name)}
-      renderInput={(params) => (
-        <TextField
-          {...params}
-          label="Search recipes"
-          slotProps={{
-            input: {
-              ...params.InputProps,
-              type: 'search',
-            },
-          }}
+    <form onSubmit={handleSubmit} className="mb-4">
+      <div className="input-group">
+        <input
+          type="text"
+          name="ingredients"
+          className="form-control"
+          placeholder="Enter ingredients (e.g., tomato, basil)"
         />
-      )}
-    />
+        <button type="submit" className="btn btn-primary">Find Recipes</button>
+      </div>
+    </form>
   );
-};
+}
 
 export default SearchBar;
-
